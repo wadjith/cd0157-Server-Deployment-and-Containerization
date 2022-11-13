4e8825fd71c435126535f1de453e1ed016dfc10e
Thierry WADJI <wadjith@yahoo.fr>
2022-11-13 15:51:42 +0100

    update main and tests

--- test_main.py
+++ test_main.py
@@ -25,8 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Healthy'
-    assert False
+    assert response.json == 'Healthy from API'
 
 
 def test_auth(client):
